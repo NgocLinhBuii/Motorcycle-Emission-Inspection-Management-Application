3a47d75dbb28c189afd2212066c2e349b7c6a8ee
NgocLinhBuii <ngoclinhh29@gmail.com>
2025-06-28 17:41:11 +0700

    update

---
+++ PRN212_Project/Motorcycle-Emission-Inspection-Management/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Motorcycle_Emission_Inspection_Management
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
